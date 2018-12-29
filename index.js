3a13fe958706edc81852977bc12629704fab5ea9
zc149352394 <149352394@qq.com>
2018-12-29 20:07:56 +0800

    更新

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const Vec3 = require('tera-vec3');
 const MapID = 9044;
 const ZoneID = 444;
 const TemplateID = [1000, 2000];
-const BossName = ['封印之门', '一阶', '二阶'];
 const {BossActionsTips} = require('./skillsList');
+const BossName = ['封印之门', '一阶', '二阶'];
 
 module.exports = function BaharrGuide(mod) {
-	var	enabled = true,
+	let	enabled = true,
 		sendToAlert = true,
 		sendToNotice = true,
 		sendToMessage = true,
 		
-		isTank = false,
-		insidemap = false,
+		isTank = true,
+		insidemap = true,
+		checkBoss = true,
 		whichboss = 0,
+		bossId = 0n,
+		
+		hooks = [],
 		
 		boss_CurLocation = null,
 		boss_CurAngle = null,
@@ -26,16 +30,15 @@ module.exports = function BaharrGuide(mod) {
 		skill = null,
 		skillid = null,
 		
-		bossIdLow = null,
-		bosshp = 1,
-		bossEnraged = false,
 		shining = false,
-		temp = true,
 		
-		uid0 = 999999999,
-		uid1 = 899999999,
-		uid2 = 799999999;
+		uid0 = 999999999n,
+		uid1 = 899999999n,
+		uid2 = 799999999n,
 		
+		itemID = 445,
+		timeOut = 0;
+	
 	mod.command.add(['巴哈', 'baha'], (arg) => {
 		if (!arg) {
 			enabled = !enabled;
@@ -83,189 +86,199 @@ module.exports = function BaharrGuide(mod) {
 		}
 	});
 	
-	mod.hook('S_LOGIN', 10, sLogin);
-	mod.hook('S_LOAD_TOPO', 3, sLoadTopo);
-	
-	mod.hook('S_ACTION_STAGE', 8, sActionStage);
-	mod.hook('S_ABNORMALITY_BEGIN', 3, sAbnormalityBegin);
-	
-	mod.hook('S_BOSS_GAGE_INFO', 3, sBossGageInfo);
-	mod.hook('S_NPC_STATUS', 1, sNpcStatus);
-	
-	function sLogin(event) {
+	mod.hook('S_LOGIN', 10, (event) => {
 		let job = (event.templateId - 10101) % 100;
 		if (job === 1 || job === 10) {					// 0-双刀, 1-枪骑, 2-大剑, 3-斧头, 4-魔道
 			isTank = true;								// 5-弓箭, 6-祭司, 7-元素, 8-飞镰, 9-魔工
 		} else {										// 10-拳师, 11-忍者 12 月光
 			isTank = false;
 		}
-	}
+	});
 	
-	function sLoadTopo(event) {
+	mod.hook('S_LOAD_TOPO', 3, (event) => {
 		if (event.zone === MapID) {
-			insidemap = true;
 			sendMessage('进入副本: ' + '火神殿 '.clr('56B4E9') + `${BossName[whichboss]}`.clr('FF0000'));
-			TEST1();
+			insidemap = true;
+			checkBoss = true;
+			load();
 		} else {
 			insidemap = false;
-			resetDungeon();
-			TEST2();
-		}
-    }
-	
-	function sActionStage(event) {
-		if (!enabled || !insidemap) return;
-		
-		if (event.stage > 0) return;
-		
-		/* if (event.templateId == 2201) {
-			if (event.skill.id == 1205) {
-				setTimeout(() => { noticeMessage('|| 小怪已苏醒 ||'.clr('FF0000')) }, 30000);
-			}
-		} */
-		
-		if (event.templateId == 2500) {
-			curLocation = event.loc;
-			curAngle = event.w;
-			
-			skill = event.skill.id % 1000;
-			/* if (skill == 201) {
-				noticeMessage('红眼射线 (小心)');
-				return;
-			} */
-			if (skill == 305) {
-				noticeMessage('红眼射线 (秒杀)');
-				Spawnitem1(912, 180, 3000, 6000);
-				return;
-			}
-		}
-		
-		if (!(TemplateID.includes(event.templateId))) return;
-		
-		boss_CurLocation = event.loc;
-		boss_CurAngle = event.w;
-		curLocation = boss_CurLocation;
-		curAngle = boss_CurAngle;
-		skillid = event.skill.id % 1000;
-		
-		if (BossActionsTips[skillid]) { noticeMessage(BossActionsTips[skillid].msg); }
-		
-		switch (skillid) {
-			case 103:	// 前砸
-			case 125:	// 右前砸+后拉
-				SpawnThing(184, 400, 100);
-				Spawnitem2(413, 8, 350, 3000);
-				break;
-				
-			case 131:	// 左前砸+后拉
-				SpawnThing(182, 340, 100);
-				Spawnitem2(413, 8, 660, 4000);
-				break;
-				
-			case 112:	// 完美格挡
-			case 135:
-				SpawnThing(184, 220, 100);
-				Spawnitem2(413, 12, 210, 4000);
-				break;
-				
-			case 114:	// 捶地
-				SpawnThing(184, 260, 100);
-				Spawnitem2(413, 10, 320, 4000);
-				break;
-				
-			case 116:	// 点名后甜甜圈
-				Spawnitem2(413, 8, 290, 6000);
-				break;
-				
-			case 111:	// 后砸 (慢慢慢慢)
-			case 137:	// 后砸
-				SpawnThing(0, 500, 100);
-				Spawnitem2(413, 8, 480, 2000);
-				break;
-				
-			case 121:	// 左脚→(4连火焰)
-			case 122:
-			case 123:
-			case 140:	// 右脚←(4连火焰)
-			case 141:
-			case 142:
-				SpawnThing(90, 45, 100);
-				Spawnitem1(413, 180, 500, 6000);
-				Spawnitem1(413, 0, 500, 6000);
-				
-				SpawnThing(270, 95, 100);
-				Spawnitem1(413, 180, 500, 6000);
-				Spawnitem1(413, 0, 500, 6000);
-				
-				setTimeout(() => { noticeMessage('四连半月 已就绪'.clr('FF0000')) }, 60000);
-				break;
-				
-			case 101:	// 锤地(三连击)
-				Spawnitem1(413, 345, 500, 4000);	// 对称轴 尾部
-				Spawnitem1(413, 270, 500, 3000);	// 对称轴 左侧
-				break;
-				
-			case 311:	// 右手放锤
-			case 312:	// 左右放锤
-				Spawnitem1(413, 180, 500, 6000);	// 对称轴 头部
-				Spawnitem1(413, 0, 500, 6000);		// 对称轴 尾部
-				break;
-				
-			case 119:	// 光柱+告示牌
-			case 120:
-				SpawnThing(BossActionsTips[skillid].sign_degrees, BossActionsTips[skillid].sign_distance, 2000);
-				break;
-				
-			default :
-				break;
-		}
-	}
-	
-	function sAbnormalityBegin(event) {
-		if (event.target.low != bossIdLow) return;
-		
-		if (event.id == 90442304) noticeMessage('以 [暈眩技能] 阻止 震怒的暴風 施展');
-		
-		if (event.id == 90442000) shining = true;
-		if (event.id == 90442001) shining = false;
-		
-		if (event.id == 90444001 && skillid == 104) setTimeout(() => { if (shining) noticeMessage('发光后砸') } , 500);
-		if (event.id == 90442000 && skillid == 134) setTimeout(() => { if (shining) noticeMessage('发光后砸') } , 300);
-		if (event.id == 90444001 && skillid == 118) setTimeout(() => { if (shining) noticeMessage('主仇->跳劈 | 发光后砸') } , 300);
-	}
-	
-	function sBossGageInfo(event) {
-		if (!enabled || !insidemap) return;
-		
-		bossIdLow = event.id;
-		bosshp = (Number(event.curHp) / Number(event.maxHp));
-		
-		if (bosshp == 1) resetDungeon();
-		
-		if (event.templateId === TemplateID[0])
-			whichboss = 1;
-		else if (event.templateId === TemplateID[1])
-			whichboss = 2;
-		else
+			checkBoss = false;
 			whichboss = 0;
+			shining = false;
+			clearInterval(timeOut);
+			unload();
+		}
+    });
+	
+	function load() {
+		if (!hooks.length) {
+			hook('S_BOSS_GAGE_INFO', 3, sBossGageInfo);
+			hook('S_ACTION_STAGE', 8, sActionStage);
+			hook('S_ABNORMALITY_BEGIN', 3, sAbnormalityBegin);
+			
+			function sBossGageInfo(event) {
+				if (!enabled || !insidemap || !checkBoss) return;
+				
+				if (event.templateId === TemplateID[0]) {
+					whichboss = 1;
+				} else if (event.templateId === TemplateID[1]) {
+					whichboss = 2;
+				} else {
+					whichboss = 0;
+				}
+				bossId = event.id;
+				checkBoss = false;
+			}
+			
+			function sActionStage(event) {
+				if (!enabled || !insidemap) return;
+				
+				if (event.stage > 0) return;
+				
+				/* if (event.templateId == 2201) {
+					if (event.skill.id == 1205) {
+						setTimeout(() => { noticeMessage('|| 小怪已苏醒 ||'.clr('FF0000')) }, 30000);
+					}
+				} */
+				
+				if (event.templateId == 2500) {
+					curLocation = event.loc;
+					curAngle = event.w;
+					
+					skill = event.skill.id % 1000;
+					/* if (skill == 201) {
+						noticeMessage('红眼射线 (小心)');
+						return;
+					} */
+					if (skill == 305) {
+						noticeMessage('<font color="#FF0000">红眼射线 (秒杀)</font>');
+						Spawnitem1(912, 180, 3000, 6000);
+						return;
+					}
+				}
+				
+				if (!(TemplateID.includes(event.templateId))) return;
+				
+				boss_CurLocation = event.loc;
+				boss_CurAngle = event.w;
+				curLocation = boss_CurLocation;
+				curAngle = boss_CurAngle;
+				skillid = event.skill.id % 1000;
+				
+				if (BossActionsTips[skillid]) { noticeMessage(BossActionsTips[skillid].msg); }
+				
+				switch (skillid) {
+					case 103:	// 前砸
+					case 125:	// 右前砸+后拉
+						SpawnThing(184, 400, 10);
+						Spawnitem2(itemID, 8, 350, 3000);
+						break;
+						
+					case 131:	// 左前砸+后拉
+						SpawnThing(182, 340, 10);
+						Spawnitem2(itemID, 8, 660, 4000);
+						break;
+						
+					case 112:	// 完美格挡
+					case 135:
+						SpawnThing(184, 220, 10);
+						Spawnitem2(itemID, 12, 210, 4000);
+						break;
+						
+					case 114:	// 捶地
+						SpawnThing(184, 260, 10);
+						Spawnitem2(itemID, 10, 320, 4000);
+						break;
+						
+					case 116:	// 点名后甜甜圈
+						Spawnitem2(itemID, 8, 290, 6000);
+						break;
+						
+					case 111:	// 后砸 (慢慢慢慢)
+					case 137:	// 后砸
+						SpawnThing(0, 500, 10);
+						Spawnitem2(itemID, 8, 480, 2000);
+						break;
+						
+					case 121:	// 左脚→(4连火焰)
+					case 122:
+					case 123:
+					case 140:	// 右脚←(4连火焰)
+					case 141:
+					case 142:
+						SpawnThing(90, 50, 10);
+						Spawnitem1(itemID, 180, 500, 6000);
+						Spawnitem1(itemID, 0, 500, 6000);
+						
+						SpawnThing(270, 100, 10);
+						Spawnitem1(itemID, 180, 500, 6000);
+						Spawnitem1(itemID, 0, 500, 6000);
+						
+						timeOut = setTimeout(() => {
+							alertMessage('四连半月 已就绪')
+							mod.send('S_CHAT', 2, {
+								channel: 25,
+								authorName: '巴哈勒',
+								message: '四连半月 已就绪'
+							});
+						}, 60000);
+						break;
+						
+					case 101:	// 锤地(三连击)
+						Spawnitem1(itemID, 345, 500, 4000);	// 对称轴 尾部
+						Spawnitem1(itemID, 270, 500, 3000);	// 对称轴 左侧
+						break;
+						
+					case 311:	// 右手放锤
+					case 312:	// 左右放锤
+						Spawnitem1(itemID, 180, 500, 6000);	// 对称轴 头部
+						Spawnitem1(itemID, 0, 500, 6000);	// 对称轴 尾部
+						break;
+						
+					case 119:	// 光柱+告示牌
+						SpawnThing(270, 300, 2000);
+						break;
+					case 120:
+						SpawnThing(90, 300, 2000);
+						break;
+						
+					default :
+						break;
+				}
+			}
+			
+			function sAbnormalityBegin(event) {
+				if (Number(event.target) != Number(bossId)) return;
+				
+				if (event.id == 90442304) noticeMessage('以 [暈眩技能] 阻止 震怒的暴風 施展');
+				
+				if (event.id == 90442000) shining = true;
+				if (event.id == 90442001) shining = false;
+				
+				if (event.id == 90444001 && skillid == 104) setTimeout(() => { if (shining) noticeMessage('发光后砸') } , 500);
+				if (event.id == 90442000 && skillid == 134) setTimeout(() => { if (shining) noticeMessage('发光后砸') } , 300);
+				if (event.id == 90444001 && skillid == 118) setTimeout(() => { if (shining) noticeMessage('发光后砸') } , 300);
+			}
+		}
 	}
 	
-	function sNpcStatus(event) {
-		if (event.creature !== bossIdLow) return;
-		
-		if (event.enraged === 1) bossEnraged = true;
-		if (event.enraged === 0) bossEnraged = false;
+	function hook() {
+		hooks.push(mod.hook(...arguments));
 	}
 	
-	function resetDungeon() {
-		whichboss = 0;
-		bossEnraged = false;
+	function unload() {
+		if (hooks.length) {
+			for (let h of hooks)
+				mod.unhook(h);
+			hooks = [];
+		}
 	}
 	
 	function alertMessage(msg) {
 		if (sendToAlert) {
 			mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
-				type: (bossEnraged ? 44 : 43),
+				type: 43,
 				chat: 0,
 				channel: 0,
 				message: msg
@@ -275,7 +288,6 @@ module.exports = function BaharrGuide(mod) {
 	
 	function noticeMessage(msg) {
 		if (sendToNotice) {
-			msg = (bossEnraged ? '愤怒 | '.clr('FF0000') : '正常 | '.clr('E69F00')) + msg
 			mod.send('S_CHAT', 2, {
 				channel: 21,
 				authorName: '巴哈勒',
@@ -289,9 +301,8 @@ module.exports = function BaharrGuide(mod) {
 	}
 	
 	function TEST1() {
-		temp = false;
 		mod.send('S_SPAWN_BUILD_OBJECT', 2, {
-			gameId : 222222222,
+			gameId : 222222222n,
 			itemId : 1,
 			loc : new Vec3(-114567, 115063, 4022),
 			w : 3,
@@ -302,9 +313,8 @@ module.exports = function BaharrGuide(mod) {
 	}
 	
 	function TEST2() {
-		temp = true;
 		mod.send('S_DESPAWN_BUILD_OBJECT', 2, {
-			gameId : 222222222,
+			gameId : 222222222n,
 			unk : 0
 		});
 	}
@@ -325,10 +335,10 @@ module.exports = function BaharrGuide(mod) {
 			w : r,
 			unk : 0,
 			ownerName : '提示',
-			message : '提示'
+			message : '安全区'
 		});
 		
-		curLocation.z = curLocation.z - 1000;
+		// curLocation.z = curLocation.z - 1000;
 		mod.send('S_SPAWN_DROPITEM', 6, {
 			gameId: uid2,
 			item: 98260,
@@ -339,7 +349,7 @@ module.exports = function BaharrGuide(mod) {
 				id: 0
 			}]
 		});
-		curLocation.z = curLocation.z + 1000;
+		// curLocation.z = curLocation.z + 1000;
 		
 		setTimeout(DespawnThing, times, uid1, uid2);
 		uid1--;
